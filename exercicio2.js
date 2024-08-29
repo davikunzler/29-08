c5669183784822e3dfa2711dcadf7e89707501ff
davikunzler <126249992+davikunzler@users.noreply.github.com>
2024-08-29 16:29:53 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,15 @@
+//1.Crie uma variável que receba e armazene a sua idade no tipo number
+let idade = 16
+
+//2.Crie uma outra variável que receba p valor da sua primeira variável, mas com o seu tipo alterado para string
+let idadeConvertidaParaString = idade.toString()
+
+//3.Imprima duas mensagens no terminal:
+//3.1.A primeira deve mostrar o valor e o tipo da primeira variável
+console.log(idade)
+console.log(typeof idade)
+
+//3.2.A segunda deve mostrar o valor e o tipo da segunda variável
+console.log(idadeConvertidaParaString)
+console.log(typeof idadeConvertidaParaString)
+
